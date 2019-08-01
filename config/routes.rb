c3b71daa808dc42a8dd0cf1608e7c4b4e270e229
setsuto horikawa <ralralph@gmail.com>
2019-08-01 17:44:03 +0900

    add: session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'users#new'
   resources :users, except:[:index]
-  resources :sessions, only:[:new]
+  resources :sessions, only:[:new, :create, :destroy]
 end
