96feec9b92f5f23017d530bcdb2e2c131f0c9b0c
setsuto horikawa <ralralph@gmail.com>
2019-08-02 13:31:17 +0900

    add: Validation, function:logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
       flash.now[:danger] = "ログインに失敗しました。"
       render 'new'
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice]= "ログアウトしました。"
+    redirect_to new_session_path
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_140318) do
+ActiveRecord::Schema.define(version: 2019_08_02_024038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_07_31_140318) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
